0e7b9f914fd2f134306d8b6dddab29eacd91ebf6
mldn <mldnqa@163.com>
2016-12-11 10:13:57 +0800

    delete

--- Action.java
+++
@@ -1,7 +0,0 @@
-public class Action 
-{
-	private int acid;
-
-	private String fff;
-	System.out.println("Ôö¼ÓAction");
-}
--- Dept.java
+++
@@ -1,9 +0,0 @@
-class Dept
-{
-	private Integer empno;
-	private String ename;
-	System.out.println("bug分支修改");
-	System.out.println("bug分支修改");
-	System.out.println("bug分支修改");
-	System.out.println("bug分支修改");
-}
