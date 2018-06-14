a1ebd02df5a6cbd4fdb312ea8780aa15746a61f6
poemfly <1046839571@qq.com>
2018-06-14 08:25:31 +0800

    add and change demo.php

--- demo/demo.php
+++ demo/demo.php
@@ -20,42 +20,9 @@
 	<title>Document</title>
 </head>
 <body>
+
+
 	Hello 你好！
-	 <h1>001</h1>
-	 <h1>002</h1>
-	 <h1>003</h1>
-	 <h1>004</h1>
-	 <h1>005</h1>
-	 <h1>006</h1>
-	 <h1>007</h1>
-	 <h1>008</h1>
-	 <h1>009</h1>
-	 <h1>010</h1>
-	 <h1>011</h1>
-	 <h1>012</h1>
-	 <h1>013</h1>
-	 <h1>014</h1>
-	 <h1>015</h1>
-	 <h1>016</h1>
-	 <h1>017</h1>
-	 <h1>018</h1>
-	 <h1>019</h1>
-	 <h1>020</h1>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h2>heloo nih hoa </h2>
-	 <h3>dddddddddd</h3>
-	 <h3>dddddddddd</h3>
-	 <h3>dddddddddd</h3>
-	 <h3>dddddddddd</h3>
-	 <h3>dddddddddd</h3>
-	 <h3>dddddddddd</h3>
+        创建分支Dev
 </body>
 </html>
->>>>>>> 5fc6b6a26de115c9249601ef60b52f036710d95d
